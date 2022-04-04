a1525ac34f03162300820e079d3f19d6f4520577
Sudhish Sepaul <sudhish.sepaul@gmail.com>
2022-04-04 14:08:38 -0500

    created a form in flask called UploadForm with a description feild and a filefeild for images only.

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,9 @@
-# Add any form classes for Flask-WTF here
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import TextAreaField
+from wtforms.validators import InputRequired, DataRequired , Length
+
+class UploadForm(FlaskForm):
+    photo = FileField('Photo ', validators=[FileRequired(),FileAllowed(['jpg', 'png'])])
+    description = TextAreaField('Description',  validators=[DataRequired(),InputRequired(),Length(max=700)])
+    
